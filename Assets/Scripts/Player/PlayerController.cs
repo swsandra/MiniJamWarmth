544be1b403d503d3402d4744ed69be5208ded931
Sandra <sandrav0609@gmail.com>
2023-01-20 21:31:04 -0400

    add player movement and stomp

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] float speed;
+    [SerializeField] float stompRate;
+    SpriteRenderer spriteRenderer;
+    Vector3 botLeft;
+    Vector3 halfWidth;
+    Rigidbody2D rb;
+    float movX;
+    float movY;
+    bool canStomp;
+
+    private void Start() {
+        rb = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        // Bottom left point of sprite
+        botLeft = spriteRenderer.transform.TransformPoint(spriteRenderer.sprite.bounds.min);
+        halfWidth = new Vector3(spriteRenderer.bounds.size.x/2, 0, 0);
+        canStomp = true;
+    }
+
+    private void FixedUpdate() {
+        rb.velocity = new Vector2(movX, movY) * speed;
+    }
+
+    private void OnMove(InputValue movementValue) {
+        Vector2 movementVector = movementValue.Get<Vector2>();
+        movX = movementVector.x;
+        movY = movementVector.y;
+    }
+
+    private void OnFire(){
+        Collider2D col = Physics2D.OverlapArea(botLeft, transform.position + halfWidth);
+        if (col && col.CompareTag("Enemy") && canStomp){ // Check Collision
+            Debug.Log("Stomp");
+            canStomp = false;
+            StartCoroutine(StompCooldown());
+        }
+        
+    }
+
+    IEnumerator StompCooldown(){
+        yield return new WaitForSeconds(1/stompRate);
+        canStomp = true;
+    }
+}
