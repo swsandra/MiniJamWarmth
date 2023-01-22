bd8b636bb259f36e2c6735fdd3d7197ae782d842
Sandra <sandrav0609@gmail.com>
2023-01-22 17:16:15 -0400

    add flash when enemies take damage

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -7,19 +7,51 @@ public class Enemy : MonoBehaviour
     public int health = 1;
     [SerializeField] GameObject explosionPrefab;
     [SerializeField] AudioClip explosionSound;
+    [SerializeField] float invincibleDuration;
+    [SerializeField] Material flashMaterial;
     CameraController camController;
+    SpriteRenderer spriteRenderer;
+    Material defaultMaterial;
+    Color originalColor;
+    Coroutine flashRoutine;
+    bool canTakeDamage;
 
     private void Start() {
         camController = FindObjectOfType<CameraController>();
-        Debug.Log("Cam controller "+camController);
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        defaultMaterial = spriteRenderer.material;
+        canTakeDamage = true;
+        originalColor = spriteRenderer.color;
+        flashRoutine = null;
     }
 
     public void TakeDamage(){
+        if (!canTakeDamage) return;
+        canTakeDamage = false;
         health--;
         camController.Shake();
         if (health <= 0){
             Kill();
+        }else{
+            Flash();
         }
+    }
+
+    void Flash(){
+        if (flashRoutine != null){
+            StopCoroutine(flashRoutine);
+        }
+        flashRoutine = StartCoroutine(FlashRoutine());
+    }
+
+    IEnumerator FlashRoutine(){
+        spriteRenderer.color = Color.white;
+        spriteRenderer.material = flashMaterial;
+        yield return new WaitForSeconds(invincibleDuration);
+        spriteRenderer.color = originalColor;
+        spriteRenderer.material = defaultMaterial;
+        flashRoutine = null;
+        canTakeDamage = true;
     }
 
     public void Kill(){
